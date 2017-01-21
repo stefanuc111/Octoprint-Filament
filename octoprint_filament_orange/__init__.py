9eb5d48ee5b4a88fde6f302e82c0b15ef18a61ed
Stefano Finocchiaro <stefanuc111@gmail.com>
2017-01-22 00:41:08 +0100

    fix

--- octoprint_filament_orange/__init__.py
+++ octoprint_filament_orange/__init__.py
@@ -52,10 +52,8 @@ class FilamentSensorPlugin(octoprint.plugin.StartupPlugin,
 			self.stop_check_loop()
 
 	def start_check_loop(self):
-		self._logger.info("Starting timer..")
 		self.stop_check_loop()
-		self._logger.info("Go...")
-		self.timer = threading.Timer(5.0, self.check_gpio, [self]).start();
+		self.timer = threading.Timer(5.0, self.check_gpio).start();
 		
 	def stop_check_loop(self):
 		try:
@@ -64,9 +62,8 @@ class FilamentSensorPlugin(octoprint.plugin.StartupPlugin,
 			pass
 
 	def check_gpio(self):
-		self._logger.info("in the loop")
 		state = gpio.input(self.PIN_FILAMENT)
-		#self._logger.info("Detected sensor state [%s]? !"%(state))
+		self._logger.info("Detected sensor state [%s]? !"%(state))
 		if state: #safety pin ?
 			self._logger.debug("Sensor [%s]!"%state)
 			if self._printer.is_printing():
@@ -94,7 +91,7 @@ class FilamentSensorPlugin(octoprint.plugin.StartupPlugin,
 		)
 
 __plugin_name__ = "Filament Sensor"
-__plugin_version__ = "1.9"
+__plugin_version__ = "1.10"
 __plugin_description__ = "Use a filament sensor to pause printing when fillament runs out."
 
 def __plugin_load__():
