f556e86ee1aa4c3164986c684d875156f0373f77
Stefano Finocchiaro <stefanuc111@gmail.com>
2017-01-21 22:15:16 +0100

    Orange PI compatibility

---
+++ octoprint_filament_orange/__init__.py
@@ -0,0 +1,106 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+import octoprint.plugin
+import octoprint.settings
+import octoprint.util
+
+from octoprint.events import eventManager, Events
+from flask import jsonify, request
+
+import logging
+import logging.handlers
+
+import threading
+
+from pyA20.gpio import gpio
+from pyA20.gpio import port
+
+class FilamentSensorPlugin(octoprint.plugin.StartupPlugin,
+							octoprint.plugin.SettingsPlugin,
+							octoprint.plugin.EventHandlerPlugin,
+							octoprint.plugin.BlueprintPlugin):
+
+	def initialize(self):
+		self._logger.setLevel(logging.DEBUG)
+
+		gpio.init()
+		
+		self._logger.info("Filament Sensor Plugin [%s] initialized..."%self._identifier)
+
+	def on_after_startup(self):
+		self.PIN_FILAMENT = port.PA6
+		gpio.setcfg(PIN_FILAMENT, gpio.INPUT)
+		gpio.pullup(button, gpio.PULLUP)
+		
+	def get_settings_defaults(self):
+		return dict(
+			pin = -1,
+		)
+
+	@octoprint.plugin.BlueprintPlugin.route("/status", methods=["GET"])
+	def check_status(self):
+		status = "1"
+		return jsonify( status = status )
+		
+	def on_event(self, event, payload):
+		if event == Events.PRINT_STARTED:
+			self._logger.info("Printing started. Filament sensor enabled.")
+			self.setup_gpio()
+		elif event in (Events.PRINT_DONE, Events.PRINT_FAILED, Events.PRINT_CANCELLED):
+			self._logger.info("Printing stopped. Filament sensor disbaled.")
+			try:
+				GPIO.remove_event_detect(self.PIN_FILAMENT)
+			except:
+				pass
+
+	def setup_gpio(self):
+			stop_check_loop(self)
+			start_check_loop(self)
+
+	def start_check_loop(self)
+		self.timer = threading.Timer(5.0, check_gpio, [self])
+		
+	def stop_check_loop(self)
+		try:
+			self.timer.cancel()
+		except:
+			pass
+
+	def check_gpio(self):
+		state = gpio.input(self.PIN_FILAMENT)
+		self._logger.debug("Detected sensor state [%s]? !"%(state))
+		if state: #safety pin ?
+			self._logger.debug("Sensor [%s]!"%state)
+			if self._printer.is_printing():
+				self._printer.toggle_pause_print()
+		self.timer.cancel();
+		start_check_loop(self)
+
+	def get_version(self):
+		return self._plugin_version
+
+	def get_update_information(self):
+		return dict(
+			octoprint_filament=dict(
+				displayName="OrangePI Filament Sensor ",
+				displayVersion=self._plugin_version,
+
+				# version check: github repository
+				type="github_release",
+				user="stefanuc111",
+				repo="OctoPrint-Filament",
+				current=self._plugin_version,
+
+				# update method: pip
+			)
+		)
+
+__plugin_name__ = "Filament Sensor"
+__plugin_version__ = "1.0"
+__plugin_description__ = "Use a filament sensor to pause printing when fillament runs out."
+
+def __plugin_load__():
+	global __plugin_implementation__
+	__plugin_implementation__ = FilamentSensorPlugin()
+
--- setup.py
+++ setup.py
@@ -4,27 +4,27 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "filament"
+plugin_identifier = "filament_orange"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_filament"
+plugin_package = "octoprint_filament_orange"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "Filament Sensor"
+plugin_name = "Filament Sensor OrangePi"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "1.0.1"
+plugin_version = "1.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
 plugin_description = """Use a filament sensor to pause printing when filament runs out."""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
-plugin_author = "ovidiu"
+plugin_author = "stefanuc111"
 
 # The plugin's author's mail address.
-plugin_author_email = "github@ovidiu.me"
+plugin_author_email = "stefanuc111@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
 plugin_url = "https://github.com/MoonshineSG/OctoPrint-Filament"
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/MoonshineSG/OctoPrint-Filament"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ["RPi.GPIO>=0.6"]
+plugin_requires = []
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
